19ff6e37fe5330d17549b5c42a5ae9fb1a82e94f
Sorawit Suriyakarn <swit@bandprotocol.com>
2019-11-29 15:26:10 +0700

    Remove unused data

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,3 @@ pub mod macros;
 
 pub mod core;
 pub mod x;
-
-decl_data! {
-  pub struct Data {
-    pub coin_gecko: f32 = x::coingecko::Price::new("bitcoin"),
-    pub crypto_compare: f32 = x::cryptocompare::Price::new(),
-  }
-}
