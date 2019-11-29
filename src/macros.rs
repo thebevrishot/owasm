9705762ac77cee22e7381c92276f355b5517a1b5
Sorawit Suriyakarn <swit@bandprotocol.com>
2019-11-29 15:55:12 +0700

    Fix rustfmt

--- src/macros.rs
+++ src/macros.rs
@@ -1,31 +1,31 @@
 #[macro_export]
 macro_rules! decl_data {
-  (pub struct $data_name:ident {
-    $(pub $field_name:ident : $field_type:ty = $field_howto:expr ,)*
-  }) => {
-    use $crate::core::{Oracle, ShellCmd, execute_with_local_env};
+    (pub struct $data_name:ident {
+        $(pub $field_name:ident : $field_type:ty = $field_howto:expr ,)*
+    }) => {
+        use $crate::core::{Oracle, ShellCmd, execute_with_local_env};
 
-    #[derive(Debug)]
-    pub struct $data_name {
-      $(pub $field_name : $field_type,)*
-    }
+        #[derive(Debug)]
+        pub struct $data_name {
+            $(pub $field_name : $field_type,)*
+        }
 
-    impl $data_name {
-      pub fn __input() -> Vec<ShellCmd> {
-        vec![ $($field_howto.as_cmd(),)* ]
-      }
+        impl $data_name {
+            pub fn __input() -> Vec<ShellCmd> {
+                vec![ $($field_howto.as_cmd(),)* ]
+            }
 
-      pub fn __output(mut output: Vec<String>) -> Option<$data_name> {
-        Some($data_name {
-          $($field_name : $field_howto.from_cmd_output(output.remove(0))?,)*
-        })
-      }
+            pub fn __output(mut output: Vec<String>) -> Option<$data_name> {
+                Some($data_name {
+                    $($field_name : $field_howto.from_cmd_output(output.remove(0))?,)*
+                })
+            }
 
-      pub fn build_from_local_env() -> Option< $data_name > {
-        Self::__output(execute_with_local_env(Self::__input()))
-      }
-    }
+            pub fn build_from_local_env() -> Option< $data_name > {
+                Self::__output(execute_with_local_env(Self::__input()))
+            }
+        }
 
-    pub type __Data = $data_name;
-  };
+        pub type __Data = $data_name;
+    };
 }
